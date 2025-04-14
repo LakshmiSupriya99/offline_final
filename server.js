8e52eeb6ab1c817314c8c96ba3a04e5b38dddc3f
Lakshmi Supriya <supriya.edu2023@gmail.com>
2025-04-14 13:09:47 -0500

    Serve frontend build and set PORT for Render

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
@@ -63,7 +64,14 @@ wss.on('connection', (ws) => {
     });
 });
 
-const PORT = 5000;
+// Serve React build
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Render-compatible port
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
